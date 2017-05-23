e9ac6671640e409136301fba036111f000d5117c
Tahzuddin <atahzuddin@yahoo.com>
2017-05-23 08:06:16 +0530

    first commit

---
+++ app/src/main/java/com/taz/movies/tmdb/tmdbmovies/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package com.taz.movies.tmdb.tmdbmovies.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.taz.movies.tmdb.tmdbmovies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
