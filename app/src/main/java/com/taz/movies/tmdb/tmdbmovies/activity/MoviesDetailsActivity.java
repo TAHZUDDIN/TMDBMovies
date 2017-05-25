f64e40a22c97cb2a15b6938852519b264bc220d0
Tahzuddin <atahzuddin@yahoo.com>
2017-05-25 08:13:58 +0530

    MoviesDetailsActivity working

--- app/src/main/java/com/taz/movies/tmdb/tmdbmovies/activity/MoviesDetailsActivity.java
+++ app/src/main/java/com/taz/movies/tmdb/tmdbmovies/activity/MoviesDetailsActivity.java
@@ -1,24 +1,101 @@
 package com.taz.movies.tmdb.tmdbmovies.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.taz.movies.tmdb.tmdbmovies.R;
+import com.taz.movies.tmdb.tmdbmovies.app.MoviesApplicationClass;
 import com.taz.movies.tmdb.tmdbmovies.pojo.Movies;
+import com.taz.movies.tmdb.tmdbmovies.pojo.MoviesDetails;
 import com.taz.movies.tmdb.tmdbmovies.util.Constants;
+import com.taz.movies.tmdb.tmdbmovies.volley.AllUser;
+import com.taz.movies.tmdb.tmdbmovies.volley.AppRequestListener;
+import com.taz.movies.tmdb.tmdbmovies.volley.BaseTask;
+import com.taz.movies.tmdb.tmdbmovies.volley.GetMoviesDetails;
 
-public class MoviesDetailsActivity extends AppCompatActivity {
+public class MoviesDetailsActivity extends AppCompatActivity  implements AppRequestListener{
 
     Movies.Movie movie;
+    MoviesDetails moviesDetails;
+    Toolbar toolbar;
+    LinearLayout LL_MainLayout;
+    RelativeLayout RL_Progressbar;
+    ImageView imageView;
+
+    TextView title, releaseDate, runtime , status, budget;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_details);
 
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        imageView=(ImageView)findViewById(R.id.image);
+        title =(TextView)findViewById(R.id.title);
+        releaseDate = (TextView)findViewById(R.id.release_date);
+        runtime = (TextView)findViewById(R.id.runtime);
+        status = (TextView)findViewById(R.id.status);
+        budget = (TextView)findViewById(R.id.budget);
+
+        LL_MainLayout =(LinearLayout)findViewById(R.id.id_LL_mainLayout);
+        RL_Progressbar =(RelativeLayout)findViewById(R.id.id_RL_progressbarDetails);
+
 
         if (getIntent().getSerializableExtra(Constants.MOVIE) != null)
             movie = (Movies.Movie) getIntent().getSerializableExtra(Constants.MOVIE);
+
+
+        if(movie != null)
+            callAPI();
     }
+
+
+
+
+    public void callAPI(){
+        String url = "https://api.themoviedb.org/3/movie/"+movie.getId()+"?api_key=d66d39af93971546a6280262deef40d6&language=en-US";
+        AllUser.getInstance().moviesDetailsGET(url, this, this);
+    }
+
+
+
+    @Override
+    public <T> void onRequestStarted(BaseTask<T> request) {
+
+        VisibilityOnOff(false);
+
+    }
+
+    @Override
+    public <T> void onRequestCompleted(BaseTask<T> request) {
+
+
+        if (((GetMoviesDetails)request).getDataObject() != null) {
+
+            moviesDetails = ((GetMoviesDetails)request).getDataObject();
+            setData();
+            VisibilityOnOff(true);
+
+        }
+
+    }
+
+    @Override
+    public <T> void onRequestFailed(BaseTask<T> request) {
+
+
+
+    }
+
 
 
     @Override
@@ -26,4 +103,38 @@ public class MoviesDetailsActivity extends AppCompatActivity {
         super.onBackPressed();
         overridePendingTransition(R.anim.slide_from_left, R.anim.slide_to_right);
     }
+
+
+    public void VisibilityOnOff(boolean mainLayoutTrueOrFalse ){
+
+        if(mainLayoutTrueOrFalse == true){
+            LL_MainLayout.setVisibility(View.VISIBLE);
+            RL_Progressbar.setVisibility(View.GONE);
+        }
+        else {
+            LL_MainLayout.setVisibility(View.GONE);
+            RL_Progressbar.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+
+
+    public void setData(){
+
+        String imageURl= "https://image.tmdb.org/t/p/w300"+moviesDetails.getBackdrop_path();
+        Picasso.with(MoviesApplicationClass.getContext())
+                .load(imageURl)
+                .placeholder(R.drawable.image_placeholder_details)
+                .error(R.drawable.image_placeholder_details)//this is also optional if some error has occurred in downloading the image this image would be displayed
+                .into(imageView);
+
+        title.setText(moviesDetails.getTitle());
+        releaseDate.setText(moviesDetails.getRelease_date());
+        runtime.setText(moviesDetails.getRuntime());
+        status.setText(moviesDetails.getStatus());
+        budget.setText(moviesDetails.getBudget());
+    }
+
+
 }
